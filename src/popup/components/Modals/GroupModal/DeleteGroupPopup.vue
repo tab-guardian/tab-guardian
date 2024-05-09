993d0063da0f7f704127f9c933d7f062b2812625
SerhiiCho <serhiicho@protonmail.com>
2024-05-09 10:27:58 +0300

    Add styles for popup

--- src/popup/components/Modals/GroupModal/DeleteGroupPopup.vue
+++ src/popup/components/Modals/GroupModal/DeleteGroupPopup.vue
@@ -17,17 +17,30 @@ const { trans } = useTransStore()
         @cancel="emit('cancel')"
     >
         <template #buttons>
-            <button @click="emit('confirm')" type="button">{{ trans('Yes') }}</button>
-            <button @click="emit('cancel')" type="button">{{ trans('No') }}</button>
+            <button @click="emit('cancel')" type="button">
+                {{ trans('No') }}
+            </button>
+
+            <button @click="emit('confirm')" type="button">
+                {{ trans('Yes') }}
+            </button>
         </template>
     </Popup>
 </template>
 
 <style scoped lang="sass">
 button
-    border: none
+    border: 1px solid var(--tg-color-border-default)
     border-radius: 4px
     padding: 7px 20px
     font-size: .9rem
     cursor: pointer
+
+    &:first-child
+        background-color: var(--tg-color-secondary)
+        color: var(--tg-color-light)
+
+    &:last-child
+        color: var(--tg-color-text-default)
+        background-color: var(--tg-color-bg-light)
 </style>
