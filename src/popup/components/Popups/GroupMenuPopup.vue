73e47404fafb3da170c37a4f22c767f972769c0c
SerhiiCho <serhiicho@protonmail.com>
2024-05-14 18:52:20 +0300

    Add warning when open menu popup in encrypted group

--- src/popup/components/Popups/GroupMenuPopup.vue
+++ src/popup/components/Popups/GroupMenuPopup.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { useTransStore } from '@/stores/trans'
 import { usePopupStore } from '@/stores/popup'
+import { useGroupStore } from '@/stores/group'
 import Popup from '@/components/Popups/Popup.vue'
 import AddLinkMenuItem from '@/components/Views/GroupView/GroupControls/MenuItems/AddLinkMenuItem.vue'
 import DeleteGroupMenuItem from '@/components/Views/GroupView/GroupControls/MenuItems/DeleteGroupMenuItem.vue'
 import RenameMenuItem from '@/components/Views/GroupView/GroupControls/MenuItems/RenameMenuItem.vue'
 import AppearTransition from '@/components/Transitions/AppearTransition.vue'
+import EnterPassword from '@/components/Views/GroupView/EnterPassword.vue'
 
 const { isOpenPopup, closePopup } = usePopupStore()
 const { trans } = useTransStore()
+const groupStore = useGroupStore()
+
+const isEncrypted = computed<boolean>(() => {
+    return (
+        !!groupStore.selectedGroup && groupStore.selectedGroup.isEncrypted
+    )
+})
 </script>
 
 <template>
@@ -18,7 +28,12 @@ const { trans } = useTransStore()
             :content="trans('Additional options')"
             @cancel="closePopup('groupView')"
         >
-            <div class="menu-items">
+            <span v-if="isEncrypted" class="warning">
+                <!-- prettier-ignore -->
+                {{ trans('This group is private. You need to unlock it to see the links') }}
+            </span>
+
+            <div v-else class="menu-items">
                 <AddLinkMenuItem />
                 <RenameMenuItem />
                 <DeleteGroupMenuItem />
@@ -33,4 +48,8 @@ const { trans } = useTransStore()
     display: flex
     flex-direction: column
     gap: 7px
+
+.warning
+    display: block
+    margin-top: 16px
 </style>
