987476a53795f4e5e5994d00c7fa6de7023d40fc
SerhiiCho <serhiicho@protonmail.com>
2024-05-18 17:52:03 +0300

    chore: Update NewGroupButton.vue styling to use rounded-lg instead of rounded-md

--- src/popup/components/Views/MainView/NewGroup/NewGroupButton.vue
+++ src/popup/components/Views/MainView/NewGroup/NewGroupButton.vue
@@ -5,7 +5,7 @@
         type="button"
         :class="[
             'disabled:opacity-50 disabled:cursor-not-allowed',
-            'h-10 flex items-center justify-center gap-2 rounded-md',
+            'h-10 flex items-center justify-center gap-2 rounded-lg',
             'text-page transition-opacity hover:opacity-90',
             'border-2 border-border',
         ]"
