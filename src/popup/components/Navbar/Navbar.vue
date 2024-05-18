13cee9c651c8c388ffad9ac5dffd02655fd4804f
SerhiiCho <serhiicho@protonmail.com>
2024-05-18 18:59:41 +0300

    Change navbar to tailwindcss

--- src/popup/components/Navbar/Navbar.vue
+++ src/popup/components/Navbar/Navbar.vue
@@ -4,37 +4,21 @@ import Sidebar from '@/components/Navbar/Sidebar/Sidebar.vue'
 </script>
 
 <template>
-    <nav class="navbar">
+    <nav
+        :class="[
+            'flex justify-between align-center h-9 bg-secondary',
+            'shadow-md py-1 px-2',
+        ]"
+    >
         <Sidebar />
 
-        <RouterLink :to="{ name: 'main' }">
-            <h1>Tab Guardian</h1>
+        <RouterLink
+            :to="{ name: 'main' }"
+            class="flex items-center transition-colors hover:bg-black/20 px-3 rounded-md text-md"
+        >
+            Tab Guardian
         </RouterLink>
 
         <NavControls />
     </nav>
 </template>
-
-<style lang="sass" scoped>
-.navbar
-    display: flex
-    justify-content: space-between
-    align-items: center
-    height: 40px
-    padding: 3px 10px
-    background-color: var(--tg-color-secondary)
-    box-shadow: 0 0 4px 5px rgb(0 0 0 / 7%)
-
-    a
-        text-decoration: none
-        color: var(--tg-color-font)
-
-    h1
-        font-size: .9rem
-        cursor: pointer
-        padding: 6px 4px
-        transition: color .2s
-
-        &:hover
-            color: var(--tg-color-primary)
-</style>
