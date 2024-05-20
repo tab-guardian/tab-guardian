13bcf765fd80752fe9ad1683bfacd2a585af1798
SerhiiCho <serhiicho@protonmail.com>
2024-05-20 15:56:18 +0300

    Fix bug with switching focus window

--- src/popup/modules/tabs/restoreTabs.ts
+++ src/popup/modules/tabs/restoreTabs.ts
@@ -1,6 +1,8 @@
 import type { Link } from '@/types'
 
-export default (links: Link[]): void => {
+export default async (links: Link[]): Promise<void> => {
+    const currWindow = await chrome.windows.getCurrent()
+
     links.forEach(link => {
         chrome.tabs.create({
             url: link.url,
@@ -8,4 +10,12 @@ export default (links: Link[]): void => {
             pinned: link.isPinned,
         })
     })
+
+    if (!currWindow.id) {
+        return
+    }
+
+    // we need to update the window to focus it because after
+    // creating tabs, the window changes focus to the other window
+    chrome.windows.update(currWindow.id, { focused: true })
 }
