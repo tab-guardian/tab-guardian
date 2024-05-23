a5c7e10f33cf748e3f5ee325f2281383ef40fe58
SerhiiCho <serhiicho@protonmail.com>
2024-05-23 16:50:49 +0300

    Rename popup

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -9,7 +9,7 @@ import Navbar from '@/components/Navbar/Navbar.vue'
 import DeleteGroupPopup from '@/components/Popups/DeleteGroupPopup.vue'
 import GroupMenuPopup from '@/components/Popups/GroupMenuPopup.vue'
 import NewGroupNamePopup from '@/components/Popups/NewGroupNamePopup.vue'
-import EnterPasswordPopup from '@/components/Popups/EnterPasswordPopup.vue'
+import EnterNewPasswordPopup from '@/components/Popups/EnterNewPasswordPopup.vue'
 import AppearTransition from '@common/components/Transitions/AppearTransition.vue'
 
 const { loadSettingsFromStorage } = useSettingsStore()
@@ -53,7 +53,7 @@ onMounted(() => {
     </AppearTransition>
 
     <AppearTransition>
-        <EnterPasswordPopup v-if="isOpenPopup('enterPassword')" />
+        <EnterNewPasswordPopup v-if="isOpenPopup('enterPassword')" />
     </AppearTransition>
 </template>
 
