2baafceca07c790d6f18fb1a864d123b19e220ea
SerhiiCho <serhiicho@protonmail.com>
2024-05-25 10:08:53 +0300

    Refactor getting and saving to storage

--- src/common/modules/storage/getFromStorage.ts
+++ src/common/modules/storage/getFromStorage.ts
@@ -1,50 +1,51 @@
 import isDevelopment from '@common/modules/isDevelopment'
+import error from '@common/modules/error'
 
-export default async <T>(key: string): Promise<T | null> => {
-    return new Promise(async (resolve) => {
-        let result = ''
+export default <T>(key: string, callback: (value: T | null) => void): void => {
+    if (isDevelopment()) {
+        callback(getFromLocalStorage<T>(key))
+        return
+    }
 
-        for (let i = 0; true; i++) {
-            let newKey = `${key}_${i}`
-
-            if (isDevelopment()) {
-                const item = localStorage.getItem(newKey)
-
-                if (!item) {
-                    break
-                }
-
-                result += item
-                continue
-            }
-
-            const item = await getFromChromeStorage(newKey)
-
-            if (!item[newKey]) {
-                break
-            }
-
-            result += item[newKey]
-        }
-
-        if (!result) {
-            resolve(null)
-            return
-        }
-
-        resolve(JSON.parse(result))
+    chrome.storage.sync.get(key, result => {
+        callback(getFromChromeStorage<T>(key, result))
     })
 }
 
-function getFromChromeStorage(key: string): Promise<{ [key: string]: any }> {
-    return new Promise((resolve, reject) => {
-        chrome.storage.sync.get(key, items => {
-            if (chrome.runtime.lastError) {
-                reject(chrome.runtime.lastError)
-                return
-            }
+function getFromChromeStorage<T>(
+    key: string,
+    result: { [key: string]: any },
+): T | null {
+    const strValue: string = result[key]
 
-            resolve(items)
-        })
-    })
+    if (!strValue) {
+        return null
+    }
+
+    const value: T | null | undefined = JSON.parse(strValue)
+
+    if (!value) {
+        error.err(`Failed to parse ${key} from storage`)
+        return null
+    }
+
+    return value
+}
+
+function getFromLocalStorage<T>(key: string): T | null {
+    const strValue: string | null = localStorage.getItem(key)
+
+    if (!strValue) {
+        error.info(`"${key}" key not found in local storage`)
+        return null
+    }
+
+    const value: T | null | undefined = JSON.parse(strValue)
+
+    if (!value) {
+        error.err(`Failed to parse ${key} from local storage`)
+        return null
+    }
+
+    return value
 }
--- src/common/modules/storage/saveToStorage.ts
+++ src/common/modules/storage/saveToStorage.ts
@@ -1,66 +1,30 @@
 import error from '@common/modules/error'
-import isDevelopment from '@common/modules/isDevelopment'
+import getBytesPerItemLimit from '@common/modules/storage/getBytesPerItemLimit'
 
 export default async <T>(
     key: string,
     value: T | null | undefined,
 ): Promise<void> => {
     if (!value) {
-        const msg = `Failed to save "${key}" to storage because value is '${value}'`
+        const msg = `Failed to save "${key}" to storage because there is not value`
         error.err(msg)
         return
     }
 
-    let json = JSON.stringify(value)
-    const limit = getBytesPerItem(key)
-    const jsonChunks = splitString(json, limit)
+    const jsonStr = JSON.stringify(value)
+    const bytesLimit = getBytesPerItemLimit(key)
+    const jsonBytes = new TextEncoder().encode(jsonStr).length
 
-    for (let i = 0; i < jsonChunks.length; i++) {
-        const chunkKey = `${key}_${i}`
-        const chunk = jsonChunks[i]
-
-        if (isDevelopment()) {
-            localStorage.setItem(chunkKey, chunk)
-            continue
-        }
-
-        await chrome.storage.sync.set({ [chunkKey]: chunk })
-    }
-}
-
-function splitString(str: string, bytesLimit: number): string[] {
-    let result = []
-    let currentChunk = ""
-    let currentBytes = 0
-
-    for (let char of str) {
-        let charBytes = new TextEncoder().encode(char).length
-
-        if (currentBytes + charBytes > bytesLimit) {
-            result.push(currentChunk)
-            currentChunk = char
-            currentBytes = charBytes
-            continue
-        }
-
-        currentChunk += char
-        currentBytes += charBytes
+    if (jsonBytes > bytesLimit) {
+        const msg = `Failed to save "${key}" to storage because it exceeds the limit of ${bytesLimit} bytes`
+        error.err(msg)
+        return
     }
 
-    if (currentChunk) {
-        result.push(currentChunk)
+    if (import.meta.env.MODE === 'development') {
+        localStorage.setItem(key, jsonStr)
+        return
     }
 
-    return result
-}
-
-// Each chunk should be something around 7786 bytes
-function getBytesPerItem(key: string): number {
-    const extraBytes = key.length + 400
-
-    if (isDevelopment()) {
-        return 8192 - extraBytes
-    }
-
-    return chrome.storage.sync.QUOTA_BYTES_PER_ITEM - extraBytes
+    await chrome.storage.sync.set({ [key]: jsonStr })
 }
