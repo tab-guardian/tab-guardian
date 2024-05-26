ecbf0f8d413d329608a665d67042f5a5c81fea79
SerhiiCho <serhiicho@protonmail.com>
2024-05-26 12:44:56 +0300

    feat: add "extension author" link to sidebar

--- src/popup/components/Navbar/Sidebar/Sidebar.vue
+++ src/popup/components/Navbar/Sidebar/Sidebar.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { useSidebarStore } from '@/stores/sidebar'
+import { useTransStore } from '@/stores/trans'
 import Hamburger from '@/components/Navbar/Sidebar/Hamburger.vue'
 import RightSlideTransition from '@common/components/Transitions/RightSlideTransition.vue'
 import Overlay from '@/components/Navbar/Sidebar/Overlay.vue'
+import UserCircleIcon from '@common/components/Icons/UserCircleIcon.vue'
+import SidebarLink from '@/components/Navbar/Sidebar/SidebarLink.vue'
 
 const sidebarStore = useSidebarStore()
+const { trans } = useTransStore()
 </script>
 
 <template>
@@ -16,9 +20,13 @@ const sidebarStore = useSidebarStore()
         <RightSlideTransition>
             <aside
                 v-if="sidebarStore.isOpen"
-                class="z-30 absolute w-64 bg-secondary inset-y-0 left-0 p-10 shadow-md"
+                class="z-30 absolute w-64 bg-secondary inset-y-0 left-0 shadow-md"
             >
-                Sidebar
+                <SidebarLink
+                    href="https://serhii.io/about-me"
+                    :label="trans('Extension Author')"
+                    :icon="UserCircleIcon"
+                />
             </aside>
         </RightSlideTransition>
     </Teleport>
---
+++ src/popup/components/Navbar/Sidebar/SidebarLink.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Component } from 'vue'
+
+type Props = {
+    label: string
+    icon: Component
+}
+
+const { label, icon } = defineProps<Props>()
+</script>
+
+<template>
+    <a
+        href="https://serhii.io/about-me"
+        target="_blank"
+        class="flex items-center gap-2.5 bg-page hover:bg-page-hover transition-colors px-4 py-2"
+    >
+        <component :is="icon" class="w-5 h-5" />
+        <span class="text-md">{{ label }}</span>
+    </a>
+</template>
