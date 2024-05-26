91622c0ef4b39c76dfe79c661d29f0d34d618462
SerhiiCho <serhiicho@protonmail.com>
2024-05-26 17:51:30 +0300

    refactor: update SidebarLink.vue styles for better responsiveness

--- src/popup/components/Navbar/Sidebar/SidebarLink.vue
+++ src/popup/components/Navbar/Sidebar/SidebarLink.vue
@@ -13,9 +13,9 @@ const { label, icon } = defineProps<Props>()
     <a
         href="https://serhii.io/about-me"
         target="_blank"
-        class="flex items-center gap-4 bg-page hover:bg-page-hover transition-colors px-4 py-2"
+        class="flex items-center gap-3 bg-page hover:bg-page-hover transition-colors px-4 py-2"
     >
         <component :is="icon" class="w-5 h-5" />
-        <span class="text-md">{{ label }}</span>
+        <span class="text-sm">{{ label }}</span>
     </a>
 </template>
