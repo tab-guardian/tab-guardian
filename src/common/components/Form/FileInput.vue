95c420e29cd7fdeb109aa825747f67e7ecc04617
SerhiiCho <serhiicho@protonmail.com>
2024-07-13 12:04:39 +0300

    WIP on file input

--- src/common/components/Form/FileInput.vue
+++ src/common/components/Form/FileInput.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-type Props = {
+import { ref } from 'vue'
+
+const props = defineProps<{
     id: string
     label?: string
-}
+}>()
 
-type Emits = {
+const emit = defineEmits<{
     (e: 'chosen', f: File, elem: HTMLInputElement): void
-}
+}>()
 
-const props = defineProps<Props>()
-const emit = defineEmits<Emits>()
+const drag = ref<boolean>(false)
 
 function onFileChange(e: Event): void {
     const target = e.target as HTMLInputElement
@@ -19,13 +20,40 @@ function onFileChange(e: Event): void {
         emit('chosen', file, e.target as HTMLInputElement)
     }
 }
+
+async function dropImage(e: DragEvent): Promise<void> {
+    e.preventDefault()
+
+    if (!e.dataTransfer) {
+        return
+    }
+
+    drag.value = false
+
+    const files = e.dataTransfer.files
+
+    console.log(files)
+}
 </script>
 
 <template>
-    <div class="border-4 border-dotted border-gray-500 rounded-lg w-96">
-        <label class="p-4 w-full block text-center">
-            <input class="hidden" @change="onFileChange" type="file" name="file" />
-            <span class="text-xl text-font-gray">{{ props.label }}</span>
+    <input
+        class="hidden"
+        @change="onFileChange"
+        type="file"
+        name="file"
+        id="file-input"
+    />
+
+    <div
+        @dragover.prevent="drag = true"
+        @drag.leave.self="drag = false"
+        @drop.self.prevent="dropImage"
+        :class="drag ? 'border-green-500' : 'border-gray-500'"
+        class="border-4 border-dotted rounded-lg w-96"
+    >
+        <label for="file-input" class="p-4 w-full block text-center">
+            <span class="text-xl text-font-gray">{{ label }}</span>
         </label>
     </div>
 </template>
