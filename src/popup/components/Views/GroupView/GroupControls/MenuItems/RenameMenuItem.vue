b0e45c6fdf7f73634f792b4d637bb612581f0b93
SerhiiCho <serhiicho@protonmail.com>
2024-11-29 09:26:41 +0200

    Refactor RenameMenuItem.vue

--- src/popup/components/Views/GroupView/GroupControls/MenuItems/RenameMenuItem.vue
+++ src/popup/components/Views/GroupView/GroupControls/MenuItems/RenameMenuItem.vue
@@ -1,32 +1,16 @@
 <script setup lang="ts">
 import { useGroupStore } from '@/stores/group'
 import { useTransStore } from '@/stores/trans'
-import { usePopupStore } from '@/stores/popup'
-import error from '@common/modules/error'
 import PencilSquareIcon from '@common/components/Icons/PencilSquareIcon.vue'
 import MenuItem from '@/components/MenuItem.vue'
 
 const store = useGroupStore()
 const { trans } = useTransStore()
-const { closePopup } = usePopupStore()
-
-function renameGroup(): void {
-    if (!store.selectedGroup) {
-        error.err('No group selected to rename')
-        return
-    }
-
-    store.isTitleFieldActive = true
-    store.selectedGroup = store.selectedGroup
-    store.newGroup.name = store.selectedGroup.name
-
-    closePopup('groupView')
-}
 </script>
 
 <template>
     <MenuItem
-        @click="renameGroup"
+        @click="store.startGroupRenaming"
         :label="trans('Rename this group')"
         :icon="PencilSquareIcon"
     />
