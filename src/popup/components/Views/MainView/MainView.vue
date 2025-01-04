4f6ac622ca7da0b02fdeb05d021a1299b5873b86
SerhiiCho <serhiicho@protonmail.com>
2025-01-04 12:29:01 +0200

    Remove NewGroup.vue

--- src/popup/components/Views/MainView/MainView.vue
+++ src/popup/components/Views/MainView/MainView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-import NewGroup from '@/components/Views/MainView/NewGroup/NewGroup.vue'
 import Groups from '@/components/Views/MainView/Groups/Groups.vue'
+import Buttons from '@/components/Views/MainView/NewGroup/Buttons.vue'
+import GroupSearch from '@/components/Views/MainView/NewGroup/GroupSearch.vue'
 </script>
 
 <template>
     <div>
-        <NewGroup />
+        <div class="text-center p-2 border-b border-border">
+            <Buttons />
+            <GroupSearch />
+        </div>
+
         <Groups />
     </div>
 </template>
