66baf92741d5afd3b1dfacdff4f4ee08b2e078b9
SerhiiCho <serhiicho@protonmail.com>
2025-02-04 15:24:00 +0200

    Move popups from app.vue to popups.vue

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { useSettingsStore } from '@/stores/settings'
-import { usePopupStore } from '@/stores/popup'
 import Navbar from '@/components/Navbar/Navbar.vue'
-import DeleteGroupPopup from '@/components/Popups/DeleteGroupPopup.vue'
-import GroupMenuPopup from '@/components/Popups/GroupMenuPopup.vue'
-import NewGroupNamePopup from '@/components/Popups/NewGroupNamePopup.vue'
-import RebindGroupPopup from '@/components/Popups/RebindGroupPopup.vue'
-import ChooseEmojiPopup from '@/components/Popups/ChooseEmojiPopup.vue'
-import ChooseImageIconPopup from '@/components/Popups/ChooseImageIconPopup.vue'
 import AppearTransition from '@common/components/Transitions/AppearTransition.vue'
+import Popups from '@/components/Popups/Popups.vue'
 
 const { loadSettingsFromStorage } = useSettingsStore()
-const { isOpenPopup } = usePopupStore()
 
 onMounted(() => {
     loadSettingsFromStorage()
@@ -34,29 +27,7 @@ onMounted(() => {
         </RouterView>
     </main>
 
-    <AppearTransition>
-        <DeleteGroupPopup v-if="isOpenPopup('deleteGroup')" />
-    </AppearTransition>
-
-    <AppearTransition>
-        <GroupMenuPopup v-if="isOpenPopup('groupView')" />
-    </AppearTransition>
-
-    <AppearTransition>
-        <NewGroupNamePopup v-if="isOpenPopup('groupName')" />
-    </AppearTransition>
-
-    <AppearTransition>
-        <RebindGroupPopup v-if="isOpenPopup('rebindGroup')" />
-    </AppearTransition>
-
-    <AppearTransition>
-        <ChooseEmojiPopup v-if="isOpenPopup('chooseEmoji')" />
-    </AppearTransition>
-
-    <AppearTransition>
-        <ChooseImageIconPopup v-if="isOpenPopup('chooseImageIcon')" />
-    </AppearTransition>
+    <Popups />
 </template>
 
 <style>
