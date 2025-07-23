c52249b2416a96d6985f139f16b7cc83aacb2d1e
SerhiiCho <serhiicho@protonmail.com>
2025-07-23 15:12:46 +0300

    chore: change message

--- src/popup/components/Views/GroupView/GroupName.vue
+++ src/popup/components/Views/GroupView/GroupName.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watchEffect, computed } from 'vue'
+import { ref, watchEffect } from 'vue'
 import type { Group } from '@/types'
 import { useGroupStore } from '@/stores/group'
 import GroupIcon from '@/components/Views/MainView/Groups/GroupIcon.vue'
