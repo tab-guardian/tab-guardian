c8433c4a44c20febd20771de8c127643babe66e8
SerhiiCho <serhiicho@protonmail.com>
2025-08-31 18:21:29 +0300

    chore: wip on app.ts store

--- src/popup/stores/app.ts
+++ src/popup/stores/app.ts
@@ -1,4 +1,4 @@
-import { Tab } from '@common/types'
+import { GroupTab } from '@/types'
 import type { Attempts } from '@/types'
 import { defineStore } from 'pinia'
 import { onMounted, ref } from 'vue'
@@ -9,10 +9,9 @@ import { error } from '@common/modules/error'
 import { showToast } from '@common/modules/showToast'
 
 export const useAppStore = defineStore('app', () => {
-    // It stores
-    const tabsBuffer = ref<Tab | null>(null)
+    // It stores copied or cut group tab
+    const tabsBuffer = ref<GroupTab | null>(null)
 
     return {
-
     }
 })
