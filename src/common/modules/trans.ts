f6eeb862255eadeeb3851d1985aff759b70f54ed
SerhiiCho <serhiicho@protonmail.com>
2025-09-09 13:43:33 +0300

    fix: trans function for development work
    Now, `trans` function can replace $n$ variables from locale messages
    with passed arguments

--- src/common/modules/trans.ts
+++ src/common/modules/trans.ts
@@ -1,7 +1,10 @@
 import { isDevelopment } from '@common/modules/isDevelopment'
 import { targetBrowser } from '@common/modules/browser/targetBrowser'
+import { error } from '@common/modules/error'
 
-let translationMessages: { [key: string]: { message: string } } | null = null
+let translationMessages: null | {
+    [key: string]: { message: string }
+} = null
 
 if (isDevelopment()) {
     const devLocale = import.meta.env.VITE_DEV_LOCALE as 'ru' | 'zh_CH' | 'en'
@@ -13,11 +16,17 @@ if (isDevelopment()) {
 export function trans(msg: string, ...args: string[]): string {
     if (translationMessages) {
         if (!translationMessages[msg]) {
-            console.warn(`English translation not found for key "${msg}"`)
+            error.warn(`English translation not found for key "${msg}"`)
             return msg
         }
 
-        return translationMessages[msg].message
+        const output = translationMessages[msg].message
+
+        if (!isDevelopment() || args.length === 0) {
+            return output
+        }
+
+        return output.replaceAll('$n$', args[0])
     }
 
     return getMessage(msg, args)
