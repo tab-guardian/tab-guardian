193dffc97f2b9745de5dd2315d6160c2cd55b978
SerhiiCho <serhiicho@protonmail.com>
2025-09-23 15:31:01 +0300

    feat: add progress bar vue component

---
+++ src/common/components/ProgressBar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+type Props = {
+    current: number
+    max: number
+}
+
+const props = defineProps<Props>()
+
+const percent = computed<number>(() => {
+    if (props.max === 0) {
+        return 0
+    }
+
+    return Math.round((props.current / props.max) * 100)
+})
+</script>
+
+<template>
+    <div class="flex gap-2 items-center w-full">
+        <div class="bg-secondary border border-border rounded-full relative overflow-hidden h-2 flex-1">
+            <div class="bg-primary absolute left-0 inset-y-0" :style="{ width: percent + '%' }"></div>
+        </div>
+
+        <small>{{ percent }}%</small>
+    </div>
+</template>
