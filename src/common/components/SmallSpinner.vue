57885b39474062d0c5f2c6d053041641eaa6b96c
SerhiiCho <serhiicho@protonmail.com>
2025-09-23 14:10:11 +0300

    feat: add small spinner comp

---
+++ src/common/components/SmallSpinner.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        class="animate-spin"
+    >
+        <circle
+            cx="12"
+            cy="12"
+            r="10"
+            fill="none"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-dasharray="15 15"
+            class="spinner-stroke-anim"
+        ></circle>
+    </svg>
+</template>
---
+++ src/common/components/Spinner.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="text-center mt-5">
+        <div class="inline-flex content-center items-center h-14 w-14">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="44"
+                height="44"
+                viewBox="0 0 44 44"
+                class="animate-spin"
+            >
+                <circle
+                    cx="22"
+                    cy="22"
+                    r="20"
+                    fill="none"
+                    stroke-width="4"
+                    stroke-linecap="round"
+                    stroke-dasharray="31.41592653589793 31.41592653589793"
+                    class="spinner-stroke-anim"
+                ></circle>
+            </svg>
+        </div>
+    </div>
+</template>
