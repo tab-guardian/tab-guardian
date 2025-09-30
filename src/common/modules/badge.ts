4117763c48151dd3fd5ba04fd5a51689eef2dc81
SerhiiCho <serhiicho@protonmail.com>
2025-09-30 16:25:42 +0300

    fix: error

--- src/common/modules/badge.ts
+++ src/common/modules/badge.ts
@@ -1,14 +1,13 @@
 import { targetBrowser } from "@common/modules/browser/targetBrowser"
 import { isDevelopment } from "./isDevelopment"
 
-const target = targetBrowser()
-
 export async function renderWarningBadge(): Promise<void> {
     if (isDevelopment()) {
         console.info('Cannot render a badge in development')
         return
     }
 
+    const target = targetBrowser()
     await target.browserAction.setBadgeText({ text: '!' })
     await target.browserAction.setBadgeBackgroundColor({ color: [245, 159, 0, 255] })
 }
@@ -19,5 +18,5 @@ export async function clearWarningBadge(): Promise<void> {
         return
     }
 
-    await target.browserAction.setBadgeText({ text: '' })
+    await targetBrowser().browserAction.setBadgeText({ text: '' })
 }
