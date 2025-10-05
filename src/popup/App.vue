ebca173a979a396f80eeed9b46ad6805066615c6
SerhiiCho <serhiicho@protonmail.com>
2025-10-05 17:50:31 +0300

    chore: improve popups.vue

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -5,6 +5,15 @@ import { useNotificationStore } from '@/stores/notification'
 import Navbar from '@/components/Navbar/Navbar.vue'
 import AppearTransition from '@common/components/Transitions/AppearTransition.vue'
 import Popups from '@/components/Popups/Popups.vue'
+import NewGroupNamePopup from '@/components/Popups/NewGroupNamePopup.vue'
+import DeleteGroupPopup from '@/components/Popups/DeleteGroupPopup.vue'
+import NewPasswordPopup from '@/components/Popups/NewPasswordPopup.vue'
+import RebindGroupPopup from '@/components/Popups/RebindGroupPopup.vue'
+import ChooseEmojiPopup from '@/components/Popups/ChooseEmojiPopup.vue'
+import ChooseImageIconPopup from '@/components/Popups/ChooseImageIconPopup.vue'
+import LinkMenuPopup from '@/components/Popups/LinkMenuPopup.vue'
+import GroupMenuPopup from '@/components/Popups/GroupMenuPopup.vue'
+import EditGroupNamePopup from '@/components/Popups/EditGroupNamePopup.vue'
 
 const { loadSettingsFromStorage } = useSettingsStore()
 const { recalculateNotification } = useNotificationStore()
@@ -33,7 +42,17 @@ function handleDarkThem(): void {
         </RouterView>
     </main>
 
-    <Popups />
+    <Popups :popups="[
+        {comp: DeleteGroupPopup, name: 'deleteGroup'},
+        {comp: NewGroupNamePopup, name: 'newGroupName'},
+        {comp: NewPasswordPopup, name: 'newPassword'},
+        {comp: RebindGroupPopup, name: 'rebindGroup'},
+        {comp: ChooseEmojiPopup, name: 'chooseEmoji'},
+        {comp: ChooseImageIconPopup, name: 'chooseImageIcon'},
+        {comp: LinkMenuPopup, name: 'linkMenuView'},
+        {comp: GroupMenuPopup, name: 'groupMenuView'},
+        {comp: EditGroupNamePopup, name: 'editGroupName'},
+    ]" />
 </template>
 
 <style>
--- src/popup/components/Popups/Popups.vue
+++ src/popup/components/Popups/Popups.vue
@@ -2,30 +2,13 @@
 import type { Component } from 'vue'
 import type { Popups } from '@/types'
 import { usePopupStore } from '@/stores/popup'
-import NewGroupNamePopup from '@/components/Popups/NewGroupNamePopup.vue'
 import AppearTransition from '@common/components/Transitions/AppearTransition.vue'
-import DeleteGroupPopup from '@/components/Popups/DeleteGroupPopup.vue'
-import NewPasswordPopup from '@/components/Popups/NewPasswordPopup.vue'
-import RebindGroupPopup from '@/components/Popups/RebindGroupPopup.vue'
-import ChooseEmojiPopup from '@/components/Popups/ChooseEmojiPopup.vue'
-import ChooseImageIconPopup from '@/components/Popups/ChooseImageIconPopup.vue'
-import LinkMenuPopup from '@/components/Popups/LinkMenuPopup.vue'
-import GroupMenuPopup from '@/components/Popups/GroupMenuPopup.vue'
-import EditGroupNamePopup from '@/components/Popups/EditGroupNamePopup.vue'
 
 const { isOpenPopup } = usePopupStore()
 
-const popups: Array<{ name: keyof Popups, comp: Component }> = [
-    {comp: DeleteGroupPopup, name: 'deleteGroup'},
-    {comp: NewGroupNamePopup, name: 'newGroupName'},
-    {comp: NewPasswordPopup, name: 'newPassword'},
-    {comp: RebindGroupPopup, name: 'rebindGroup'},
-    {comp: ChooseEmojiPopup, name: 'chooseEmoji'},
-    {comp: ChooseImageIconPopup, name: 'chooseImageIcon'},
-    {comp: LinkMenuPopup, name: 'linkMenuView'},
-    {comp: GroupMenuPopup, name: 'groupMenuView'},
-    {comp: EditGroupNamePopup, name: 'editGroupName'},
-]
+defineProps<{
+    popups: Array<{ name: keyof Popups, comp: Component }>
+}>()
 </script>
 
 <template>
