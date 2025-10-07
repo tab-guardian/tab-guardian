c9c54ad60a1327a0a2fa469f7f815c743067d443
SerhiiCho <serhiicho@protonmail.com>
2025-10-07 20:00:13 +0300

    refactor: add comments

--- src/common/modules/webCrypto.ts
+++ src/common/modules/webCrypto.ts
@@ -65,14 +65,21 @@ export async function encrypt(
     return new Uint8Array(encrypted)
 }
 
+// Converts Uint8Array to Base64 string
+// Or: Converts bytes to Base64 encoded string.
+// Use new TextEncoder().encode() to turn a string to a UTF-8
+// array of bytes.
 export function toBase64(arr: Uint8Array): string {
-    const binary = Array.from(arr)
+    // Latin-1 encoding covers the 0-255 range
+    const latin1String = Array.from(arr)
         .map(byte => String.fromCharCode(byte))
         .join('')
 
-    return btoa(binary)
+    return btoa(latin1String)
 }
 
+// Decodes Base64 string back to bytes.
+// Use new TextDecoder().decode() to turn it to a UTF-8 string
 export function fromBase64(str: string): Uint8Array {
     return Uint8Array.from(atob(str), c => c.charCodeAt(0))
 }
