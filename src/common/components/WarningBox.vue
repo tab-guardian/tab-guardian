2e329b4d13f31b7db4a0c555c1e360a37238b889
SerhiiCho <serhiicho@protonmail.com>
2025-10-13 14:02:26 +0300

    chore: improve WarningBox.vue

--- src/common/components/WarningBox.vue
+++ src/common/components/WarningBox.vue
@@ -1,15 +1,38 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import ShieldExclamationIcon from '@common/components/Icons/ShieldExclamationIcon.vue'
+import ShieldCheckIcon from '@common/components/Icons/ShieldCheckIcon.vue';
 
-defineProps<{ message: string }>()
+type Props = {
+    message: string
+    success?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    success: false,
+})
+
+const boxClassName = computed<string>(() => {
+    return props.success ? 'bg-safe' : 'bg-unsafe'
+})
 </script>
 
 <template>
     <div
-        class="flex items-center justify-between mb-4 mt-1 bg-unsafe p-3 rounded-lg gap-4"
+        class="flex items-center justify-between mb-4 mt-1 p-3 rounded-lg gap-4"
+        :class="boxClassName"
     >
-        <ShieldExclamationIcon class="w-8 h-8 text-red-400 flex-shrink-0" />
-        <span class="text-sm">{{ message }}</span>
+        <ShieldCheckIcon
+            v-if="success"
+            class="text-green-400 w-8 h-8 flex-shrink-0"
+        />
+
+        <ShieldExclamationIcon
+            v-else
+            class="text-red-400 w-8 h-8 flex-shrink-0"
+        />
+
+        <span class="text-sm w-full">{{ message }}</span>
         <slot />
     </div>
 </template>
