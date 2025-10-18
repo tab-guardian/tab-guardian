b5b631d5d21734d9a733b7dad91affdb4ca226eb
SerhiiCho <serhiicho@protonmail.com>
2025-10-18 12:10:41 +0300

    refactor: rewrite hasURL to use web crypto API

--- src/common/modules/url/hashURL.ts
+++ src/common/modules/url/hashURL.ts
@@ -1,6 +1,14 @@
-import CryptoJS from 'crypto-js'
+import { removeTrail } from '@common/modules/utils'
 
-export function hashURL(url: string): string {
-    const trimmedURL = url.replace(/\/$/, '')
-    return CryptoJS.SHA256(trimmedURL).toString().trim()
+export async function hashURL(url: string): Promise<string> {
+    const trimmedURL = removeTrail(url, '/')
+
+    const arrBuffer = await crypto.subtle.digest(
+        'SHA-256',
+        new TextEncoder().encode(trimmedURL),
+    )
+
+    return Array.from(new Uint8Array(arrBuffer))
+        .map(byte => byte.toString(16).padStart(2, '0'))
+        .join('')
 }
