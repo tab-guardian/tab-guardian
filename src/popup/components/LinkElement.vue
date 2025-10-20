1ab9341158388b398f58239cb814cd533a78b737
SerhiiCho <serhiicho@protonmail.com>
2025-10-20 12:22:52 +0300

    refactor(LinkElement.vue)

--- src/popup/components/LinkElement.vue
+++ src/popup/components/LinkElement.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import type { Link } from '@common/types'
 import { computed } from 'vue'
-import LinkElementInner from '@/components/LinkElementInner.vue'
 
 const props = defineProps<{
     link: Link
@@ -30,7 +29,26 @@ const classes = computed(() => {
 
 <template>
     <div :class="classes">
-        <LinkElementInner :link />
+        <div class="flex gap-3 items-center">
+            <img :src="link.favIconUrl" alt="icon" class="size-7 rounded-md" />
+
+            <div class="flex flex-col w-72 min-w-0">
+                <h2
+                    :title="link.title"
+                    class="text-sm leading-4 overflow-hidden text-ellipsis text-nowrap"
+                >
+                    {{ link.title }}
+                </h2>
+
+                <p
+                    :title="link.url"
+                    class="overflow-hidden text-ellipsis text-nowrap text-xs text-font-gray"
+                >
+                    {{ link.url }}
+                </p>
+            </div>
+        </div>
+
         <slot />
     </div>
 </template>
