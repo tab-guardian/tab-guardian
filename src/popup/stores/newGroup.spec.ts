5fd09d715db3182736986857d53e37eaab365b9a
SerhiiCho <serhiicho@protonmail.com>
2025-10-25 10:17:00 +0300

    test: add tests to newGroup store

---
+++ src/popup/stores/newGroup.spec.ts
@@ -0,0 +1,133 @@
+// @vitest-environment happy-dom
+
+import { describe, it, expect, suite, beforeEach } from 'vitest'
+import { createPinia, setActivePinia } from 'pinia'
+import { useNewGroupStore } from '@/stores/newGroup'
+import { fakeLink } from '@common/modules/fake'
+
+describe('newGroupStore', () => {
+    beforeEach(() => {
+        localStorage.clear()
+        setActivePinia(createPinia())
+    })
+
+    it('choices are null by default', () => {
+        const store = useNewGroupStore()
+
+        for (const key in store.choices) {
+            // @ts-ignore
+            expect(store.choices[key]).toBeNull()
+        }
+    })
+
+    suite('nameLength', () => {
+        it('returns the length of the group name', () => {
+            const store = useNewGroupStore()
+            const groupName = 'Amy Adams Movies'
+
+            store.choices.name = groupName
+
+            expect(store.nameLength).equal(groupName.length)
+        })
+
+        it('returns 0 when name is null', () => {
+            const store = useNewGroupStore()
+            expect(store.nameLength).equal(0)
+        })
+
+        it('returns 0 for empty string', () => {
+            const store = useNewGroupStore()
+            store.choices.name = ''
+            expect(store.nameLength).equal(0)
+        })
+    })
+
+    suite('createGroupFromChoices()', () => {
+        it('creates group from provided choices', () => {
+            const store = useNewGroupStore()
+            const name = 'Amy Adams Movies'
+            const bindUrl = 'https://amy-adams.org'
+
+            store.choices.name = name
+            store.choices.isPrivate = true
+            store.choices.bindUrl = bindUrl
+
+            const group = store.createGroupFromChoices([fakeLink(), fakeLink()])
+
+            expect(group.name).equal(name)
+            expect(group.isPrivate).toBeTruthy()
+            expect(group.bindUrl).equal(bindUrl)
+            expect(group.updatedAt).toBeTypeOf('number')
+            expect(group.createdAt).toBeTypeOf('number')
+            expect(group.links).toHaveLength(2)
+        })
+
+        it('does not add bindUrl field when not provided', () => {
+            const store = useNewGroupStore()
+            const name = 'Sydney Sweeney Movies'
+
+            store.choices.name = name
+            store.choices.isPrivate = true
+
+            const group = store.createGroupFromChoices([fakeLink()])
+
+            expect(group.name).equal(name)
+            expect(group.isPrivate).toBeTruthy()
+            expect(group.bindUrl).toBeUndefined()
+        })
+    })
+
+    suite('isPasswordEmpty()', () => {
+        it('returns false for empty password for non-private group', () => {
+            const store = useNewGroupStore()
+            store.choices.password = ''
+            store.choices.isPrivate = false
+
+            expect(store.isPasswordEmpty()).toBeFalsy()
+        })
+
+        it('returns false for empty password when isPrivate equal to null', () => {
+            const store = useNewGroupStore()
+            store.choices.password = ''
+            store.choices.isPrivate = null
+
+            expect(store.isPasswordEmpty()).toBeFalsy()
+        })
+
+        it('returns true for empty password for private group', () => {
+            const store = useNewGroupStore()
+            store.choices.password = ''
+            store.choices.isPrivate = true
+
+            expect(store.isPasswordEmpty()).toBeTruthy()
+        })
+
+        it('returns true for null password for private group', () => {
+            const store = useNewGroupStore()
+            store.choices.password = null
+            store.choices.isPrivate = true
+
+            expect(store.isPasswordEmpty()).toBeTruthy()
+        })
+    })
+
+    suite('resetChoices()', () => {
+        it('choices are null by default', () => {
+            const store = useNewGroupStore()
+
+            store.choices.name = 'Amy Adams Movies'
+            store.choices.isPrivate = true
+            store.choices.bindUrl = 'https://amy-adams.org'
+            store.choices.closeTabs = false
+            store.choices.password = 'sydney-sweeney'
+            store.choices.wantsSelectAllLinks = true
+
+            store.resetChoices()
+
+            for (const key in store.choices) {
+                // @ts-ignore
+                expect(store.choices[key]).toBeNull()
+            }
+        })
+    })
+})
--- src/popup/stores/newGroup.ts
+++ src/popup/stores/newGroup.ts
@@ -1,12 +1,12 @@
 import type { Group, UserChoices, Link } from '@common/types'
-import { ref, computed } from 'vue'
+import { reactive, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { getDefaultGroupName } from '@common/modules/group'
 import { generateGroupId } from '@common/modules'
 
 export const useNewGroupStore = defineStore('newGroup', () => {
     // null suggest that there was no choice yet
-    const choices = ref<UserChoices>({
+    const choices = reactive<UserChoices>({
         isPrivate: null,
         name: null,
         closeTabs: null,
@@ -17,43 +17,43 @@ export const useNewGroupStore = defineStore('newGroup', () => {
     })
 
     const nameLength = computed<number>(() => {
-        choices.value.name ??= ''
-        return choices.value.name.length
+        choices.name ??= ''
+        return choices.name.length
     })
 
     function createGroupFromChoices(links: Link[]): Group {
         const group: Group = {
             id: generateGroupId(),
-            name: choices.value.name || getDefaultGroupName(),
-            isPrivate: choices.value.isPrivate || false,
+            name: choices.name || getDefaultGroupName(),
+            isPrivate: choices.isPrivate || false,
             isEncrypted: false,
             updatedAt: Date.now(),
             createdAt: Date.now(),
             links,
         }
 
-        if (choices.value.bindUrl) {
-            group.bindUrl = choices.value.bindUrl
+        if (choices.bindUrl) {
+            group.bindUrl = choices.bindUrl
         }
 
         return group
     }
 
     function isPasswordEmpty(): boolean {
-        choices.value.isPrivate ??= false
-        return choices.value.isPrivate && !choices.value.password
+        choices.isPrivate ??= false
+        return choices.isPrivate && !choices.password
     }
 
     function resetChoices(): void {
-        for (const key in choices.value) {
+        for (const key in choices) {
             // @ts-ignore
-            choices.value[key] = null
+            choices[key] = null
         }
     }
 
     return {
-        nameLength,
         choices,
+        nameLength,
         createGroupFromChoices,
         isPasswordEmpty,
         resetChoices,
