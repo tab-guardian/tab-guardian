6b8afc0a7888c8d0a7f54414ca5bc0797b59246c
SerhiiCho <serhiicho@protonmail.com>
2025-10-26 17:41:00 +0200

    test: added tests to group storage module

--- src/common/modules/storage/group.spec.ts
+++ src/common/modules/storage/group.spec.ts
@@ -2,10 +2,13 @@
 
 import { describe, it, expect, suite, beforeEach } from 'vitest'
 import { createPinia, setActivePinia } from 'pinia'
-import { saveGroupToStorage } from '@common/modules/storage/group'
-import { fakeGroup } from '@common/modules/fake'
+import {
+    deleteGroupFromStorage,
+    saveGroupToStorage,
+} from '@common/modules/storage/group'
+import { fakeGroup, fakeLink } from '@common/modules/fake'
 
-describe('storage/group', () => {
+describe('group storage module', () => {
     beforeEach(() => {
         localStorage.clear()
         setActivePinia(createPinia())
@@ -13,7 +16,7 @@ describe('storage/group', () => {
 
     suite('saveGroupToStorage()', () => {
         it('saves group into storage', async () => {
-            const group = fakeGroup()
+            const group = fakeGroup({ links: [fakeLink()] })
             await saveGroupToStorage(group)
 
             const itemStr = localStorage.getItem(group.id.toString())
@@ -24,6 +27,48 @@ describe('storage/group', () => {
 
             expect(itemParsed.id).equal(group.id)
             expect(itemParsed.name).equal(group.name)
+            expect(itemParsed.links).toHaveLength(1)
+        })
+
+        it('adds group ID to groupIds storage', async () => {
+            const group = fakeGroup()
+            await saveGroupToStorage(group)
+
+            const idsStr = localStorage.getItem('groupIds')
+
+            expect(idsStr).not.toBeNull()
+
+            const idsParsed = JSON.parse(idsStr!)
+
+            expect(idsParsed).toBeTypeOf('object')
+            expect(idsParsed).toHaveLength(1)
+            expect(idsParsed[0]).equal(group.id)
+        })
+    })
+
+    suite('deleteGroupFromStorage()', () => {
+        it('deletes group from storage', async () => {
+            const group = fakeGroup()
+            await saveGroupToStorage(group)
+
+            await deleteGroupFromStorage(group.id)
+
+            const foundItem = localStorage.getItem(group.id.toString())
+
+            expect(foundItem).toBeNull()
+        })
+
+        it('deletes group ID from groupIds', async () => {
+            const group = fakeGroup()
+            await saveGroupToStorage(group)
+
+            await deleteGroupFromStorage(group.id)
+
+            const idsStr = localStorage.getItem('groupIds')
+            const idsParsed = JSON.parse(idsStr!)
+
+            expect(idsParsed).toBeTypeOf('object')
+            expect(idsParsed).toHaveLength(0)
         })
     })
 })
