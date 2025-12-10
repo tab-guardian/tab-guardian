0f855bcaa049038eaf8f471dd3aec66bf9c0086d
SerhiiCho <serhiicho@protonmail.com>
2025-12-10 12:55:17 +0200

    feat: add ability to drag&drop groups to folders
    wip on #47

--- src/popup/components/Views/MainView/ListItems/ListItems.vue
+++ src/popup/components/Views/MainView/ListItems/ListItems.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import type { Group, Folder } from '@common/types'
+import { computed } from 'vue'
 import { trans } from '@common/modules'
 import { useGroupStore } from '@/stores/group'
+import { VueDraggableNext } from 'vue-draggable-next'
 import GroupItem from '@/components/Views/MainView/ListItems/GroupItem.vue'
 import Message from '@common/components/Message.vue'
 import Spinner from '@common/components/Spinner.vue'
@@ -17,6 +19,26 @@ type Props = {
 const props = withDefaults(defineProps<Props>(), {
     folders: () => [],
 })
+
+const groups = computed(() => props.groups)
+
+function moveToFolder(e: any): void {
+    const folderElem = e.explicitOriginalTarget as HTMLElement
+    const groupElem = e.item as HTMLElement
+
+    const folderValue = folderElem.dataset.folder
+    const groupValue = groupElem.dataset.group
+
+    if (!folderValue || !groupValue) {
+        return
+    }
+
+    const folderId = Number(folderValue)
+    const groupId = Number(groupValue)
+
+    groupStore.update(groupId, { folderId })
+    groupStore.load()
+}
 </script>
 
 <template>
@@ -26,11 +48,23 @@ const props = withDefaults(defineProps<Props>(), {
         {{ trans('no_groups_yet') }}
     </Message>
 
-    <div v-else class="flex flex-col">
-        <FolderItem v-for="folder in folders" :key="folder.name" :folder />
+    <div v-else class="flex flex-col border-t border-border">
+        <VueDraggableNext
+            :sort="false"
+            @end="moveToFolder"
+            v-model="groups"
+            item-key="id"
+        >
+            <FolderItem
+                v-for="folder in folders"
+                :key="folder.id"
+                :data-folder="folder.id"
+                :folder
+            />
 
-        <div v-for="group in groups" :key="group.id">
-            <GroupItem v-if="!group.hide" :group />
-        </div>
+            <div v-for="group in groups" :key="group.id" :data-group="group.id">
+                <GroupItem v-if="!group.hide" :group />
+            </div>
+        </VueDraggableNext>
     </div>
 </template>
--- src/popup/components/Views/MainView/ListItems/MainItem.vue
+++ src/popup/components/Views/MainView/ListItems/MainItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div
         :class="[
-            'first:border-t p-2 flex justify-between items-center gap-3',
+            'p-2 flex justify-between items-center gap-3',
             'transition-colors border-b border-border',
             'cursor-pointer bg-page hover:bg-page-hover',
         ]"
