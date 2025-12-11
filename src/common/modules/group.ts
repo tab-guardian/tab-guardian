6a470b0840142fdf6f761c4ae0e12addd1aff8d9
SerhiiCho <serhiicho@protonmail.com>
2025-12-11 10:23:22 +0200

    test: add tests to isComponentIcon module

--- src/common/modules/group.ts
+++ src/common/modules/group.ts
@@ -15,6 +15,10 @@ export function generateId(): number {
 }
 
 export function isComponentIcon(str: string): boolean {
+    if (str.length <= 4) {
+        return false
+    }
+
     return str.slice(-4) === 'Icon'
 }
 
