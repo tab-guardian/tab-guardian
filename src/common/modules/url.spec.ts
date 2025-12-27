0d8de78ce2d25ade216cc6f01cadebdb07c780ea
SerhiiCho <serhiicho@protonmail.com>
2025-12-27 18:41:05 +0200

    chore: add 2 tests

--- src/common/modules/url.spec.ts
+++ src/common/modules/url.spec.ts
@@ -6,6 +6,7 @@ import {
     getCurrentUrl,
     getHashedCurrentUrl,
     hashUrl,
+    isForbittenUrl,
     isImageUrl,
 } from '@common/modules/url'
 import { fakeInvalidImageUrls, fakeValidImageUrls } from './fake'
@@ -60,6 +61,13 @@ describe('url utilities module', () => {
             for (const url of fakeInvalidImageUrls()) {
                 expect(isImageUrl(url), `${url} must be invalid`).toBeFalsy()
             }
+        })
+    })
+
+    suite('isForbittenUrl()', () => {
+        it('returns false because the runtime is not firefox', () => {
+            const res = isForbittenUrl('https://duckduckgo.com/')
+            expect(res).toBeFalsy()
         })
     })
 })
